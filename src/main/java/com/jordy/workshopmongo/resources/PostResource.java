6c00870c2118c209a41088122d7768af766a263a
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-05-17 19:42:31 -0300

    consulta simples com param methods

--- src/main/java/com/jordy/workshopmongo/resources/PostResource.java
+++ src/main/java/com/jordy/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.jordy.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jordy.workshopmongo.domain.Post;
+import com.jordy.workshopmongo.resources.util.URL;
 import com.jordy.workshopmongo.services.PostService;
 
 @RestController
@@ -18,9 +22,19 @@ public class PostResource {
 	private PostService service;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	//pra casar o id recebido na url com o campo, usar pathvariable
-	public ResponseEntity<Post> findById(@PathVariable String id){
+	// pra casar o id recebido na url com o campo, usar pathvariable
+	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	// como vai ser parametro ?parametro=... mudar pathvvariable para RequestParam.
+	// Value mostra ao parametro para entender qual tem que procurar, no caso "text="
+	// se o valor nao for informado, 'default' atribui ""
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);	
+		return ResponseEntity.ok().body(list);
 	}
 }
