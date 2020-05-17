edc590f529ed12e6a87262b099202d91407076e7
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-05-17 19:59:43 -0300

    corrigindo a bagunça que o commit errado gerou haha - consulta simples com ->query<- methods

--- src/main/java/com/jordy/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/jordy/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.jordy.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import com.jordy.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
-	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	//busca utilizando query methods, só esta declaração já faz o spring data montar a consulta
+	//modificando o nome da função colocando 'IgnoreCase' deixa de casesensitive
 }//tudo do crud ja esta imbutido no mongorepository
--- src/main/java/com/jordy/workshopmongo/services/PostService.java
+++ src/main/java/com/jordy/workshopmongo/services/PostService.java
@@ -1,4 +1,4 @@
- package com.jordy.workshopmongo.services;
+package com.jordy.workshopmongo.services;
 
 import java.util.List;
 import java.util.Optional;
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jordy.workshopmongo.domain.Post;
-import com.jordy.workshopmongo.domain.User;
-import com.jordy.workshopmongo.dto.UserDTO;
 import com.jordy.workshopmongo.repository.PostRepository;
-import com.jordy.workshopmongo.repository.UserRepository;
 import com.jordy.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
@@ -22,6 +19,10 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String title){
+		return repo.findByTitleContainingIgnoreCase(title);
 	}
 
 	
