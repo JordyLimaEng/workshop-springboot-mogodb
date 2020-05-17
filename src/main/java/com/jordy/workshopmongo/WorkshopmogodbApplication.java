6f9f64e4a7493c6e2ddfaf9c135121741687a2f1
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-05-17 14:22:39 -0300

    projeto criado

---
+++ src/main/java/com/jordy/workshopmongo/WorkshopmogodbApplication.java
@@ -0,0 +1,13 @@
+package com.jordy.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmogodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmogodbApplication.class, args);
+	}
+
+}
