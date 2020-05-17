12af3a74c34da5a3f37ea4328f0facfed6f866a3
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-05-17 18:50:31 -0300

    obtendo post por id

---
+++ src/main/java/com/jordy/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.jordy.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jordy.workshopmongo.domain.Post;
+import com.jordy.workshopmongo.domain.User;
+import com.jordy.workshopmongo.dto.UserDTO;
+import com.jordy.workshopmongo.repository.PostRepository;
+import com.jordy.workshopmongo.repository.UserRepository;
+import com.jordy.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired // o proprio spring procura a declaração do objeto e já declara aqui
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	
+}
